ddf2ae1307489f6588eb085c24086fd28ef3c559
Sati <s.urkumbayeva@mail.ru>
2023-08-09 01:29:17 +0600

    Studied Hooks and overriding templates

--- wp-content/themes/eshop/functions.php
+++ wp-content/themes/eshop/functions.php
@@ -5,4 +5,31 @@
 add_action( 'after_setup_theme', function() {
 
     add_theme_support( 'woocommerce' );
+
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+
+    add_theme_support( 'title-tag' );
 });
+
+// change content wrapper
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+// add action
+
+add_action('woocommerce_before_main_content', function() {
+
+    echo '<div class="content-area test-container2">';
+});
+
+add_action('woocommerce_after_main_content', function() {
+
+    echo '</div>';
+});
+
+remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
+
+add_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 9);
